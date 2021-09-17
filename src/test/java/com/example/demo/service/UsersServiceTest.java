0f259a353f6f5229d4f463515d917666cb68a50e
shiryu1027 <asmttmcz.1027@outlook.jp>
2021-09-17 22:38:40 +0900

    UsersServiceクラスのテスト追加
    暗号化については、モックを使わないやり方を検討要

---
+++ src/test/java/com/example/demo/service/UsersServiceTest.java
@@ -0,0 +1,158 @@
+package com.example.demo.service;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoSession;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.example.demo.entity.AppUser;
+import com.example.demo.form.UserForm;
+import com.example.demo.mapper.UsersMapper;
+
+@SpringBootTest
+class UsersServiceTest {
+	
+	private MockitoSession session;
+	
+	@Mock
+	UsersMapper mapper;
+	
+	@Mock
+	PasswordEncoder encoder;
+	
+	@InjectMocks
+	UsersService target;
+	
+	@BeforeEach
+	void setup() {
+		session = Mockito.mockitoSession().initMocks(this).startMocking();
+	}
+	
+	@AfterEach
+	void tearDown() {
+	    session.finishMocking();
+	}
+	
+	@Nested
+	class ユーザーの新規登録 {
+		
+		private UserForm userForm;
+		
+		@BeforeEach
+		void setup() {
+			userForm = new UserForm();
+			userForm.setUserId(1);
+			userForm.setMailAddress("user@co.jp");
+			userForm.setNickname("ユーザー");
+			userForm.setPassword("password");
+			doNothing().when(mapper).signUp(userForm);
+			doReturn("encryptionPassword").when(encoder).encode("password");
+			target.signUp(userForm);
+		}
+		
+		@Test
+		void パスワードを暗号化するメソッドが呼ばれている() throws Exception {
+			verify(encoder,times(1)).encode("password");
+		}
+		
+		@Test
+		void roleにROLE_GENERALが追加されている() throws Exception {
+			assertThat(userForm.getRole()).isEqualTo("ROLE_GENERAL");
+		}
+		
+		@Test
+		void UserMapperのsignUpメソッドが一回呼ばれる() throws Exception {
+			verify(mapper,times(1)).signUp(userForm);
+		}
+	}
+	
+	@Nested
+	class サインイン情報の取得 {
+		
+		private String mailAddress;
+		private AppUser appUser;
+		private AppUser actual;
+		
+		@BeforeEach
+		void setup() {
+			mailAddress = "user@co.jp";
+			appUser = new AppUser();
+			appUser.setUserId(1);
+			appUser.setMailAddress("user@co.jp");
+			appUser.setNickname("ユーザー");
+			appUser.setPassword("password");
+			doReturn(appUser).when(mapper).findOne(mailAddress);
+			actual = target.getSignInUser(mailAddress);
+		}
+		
+		@Test
+		void UserMapperのfindOneメソッドが一回呼ばれる() throws Exception {
+			
+			verify(mapper,times(1)).findOne(mailAddress);
+		}
+		
+		@Test
+		void メールアドレスからAppUser型のユーザーデータを返す() throws Exception {
+			assertThat(actual.getUserId()).isEqualTo(1);
+		}
+	}
+	
+	@Nested
+	class ニックネームの取得 {
+		
+		private String mailAddress;
+		private String nickname;
+		String actual;
+		
+		@BeforeEach
+		void setup() {
+			mailAddress = "user@co.jp";
+			nickname = "ユーザー";
+			doReturn(nickname).when(mapper).getUserNickname(mailAddress);
+			actual = target.getUserNickname(mailAddress);
+		}
+		
+		@Test
+		void UserMapperのgetUserNicknameメソッドが一回呼ばれる() throws Exception {
+			
+			verify(mapper,times(1)).getUserNickname(mailAddress);
+		}
+		
+		@Test
+		void mailAddressからニックネームが取得できる() throws Exception {
+			assertThat(actual).isEqualTo(nickname);
+		}
+	}
+	
+	@Nested
+	class ユーザー情報更新 {
+		
+		private UserForm userForm;
+		
+		@BeforeEach
+		void setup() {
+			userForm = new UserForm();
+			userForm.setUserId(1);
+			userForm.setMailAddress("user@co.jp");
+			userForm.setNickname("ユーザー");
+			userForm.setPassword("password");
+			userForm.setRole("ROLE_GENERAL");
+			doNothing().when(mapper).updateUser(userForm);
+			target.updateUser(userForm);
+		}
+		
+		@Test
+		void UserMapperのupdateUserメソッドが一回呼ばれる() throws Exception {
+			verify(mapper,times(1)).updateUser(userForm);
+		}
+	}
+}
