865290c6022b12b8d8e7004732aae25729becf91
shiryu1027 <asmttmcz.1027@outlook.jp>
2021-09-26 22:11:16 +0900

    年月日のvalidation変更

--- src/main/java/com/example/demo/form/DateFormulaForm.java
+++ src/main/java/com/example/demo/form/DateFormulaForm.java
@@ -13,17 +13,17 @@ public class DateFormulaForm {
 	
 	private String additionSubtractionCode;
 	
-	@Max(value=100000, message="年には100,000以下の値を入力して下さい")
-	@Min(value=-100000, message="年には-100,000以上の値を入力して下さい")
+	@Max(value=2500, message="年には2,500以下の値を入力して下さい")
+	@Min(value=-2500, message="年には-2,500以上の値を入力して下さい")
 	private int year;
 	/* LocalDateのYearは10億年を超えると、エラーが起きる(10万年でも多すぎるくらい) */
 	
-	@Max(value=100000, message="月には100,000以下の値を入力して下さい")
-	@Min(value=-100000, message="月には-100,000以上の値を入力して下さい")
+	@Max(value=2500, message="月には2,500以下の値を入力して下さい")
+	@Min(value=-2500, message="月には-2,500以上の値を入力して下さい")
 	private int month;
 	
-	@Max(value=100000, message="日には100,000以下の値を入力して下さい")
-	@Min(value=-100000, message="日には-100,000以上の値を入力して下さい")
+	@Max(value=2500, message="日には2,500以下の値を入力して下さい")
+	@Min(value=-2500, message="日には-2,500以上の値を入力して下さい")
 	private int day;
 	
 	@NotBlank(message="説明は必須項目です")
