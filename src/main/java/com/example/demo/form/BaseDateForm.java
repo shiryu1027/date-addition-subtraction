53353ee953980d93fc869792caa78bb4fe7e03b8
shiryu1027 <asmttmcz.1027@outlook.jp>
2021-10-04 23:23:52 +0900

    BaseDateFormのテスト断念
    理由
    (1)SpringSecurityのバリデーションなので、他のバリデーション同様にチェックできない
    (2)Controllerクラスからも、formにセットできないので実装できず(LocalDate型なので、そもそもはじかれる)

--- src/main/java/com/example/demo/form/BaseDateForm.java
+++ src/main/java/com/example/demo/form/BaseDateForm.java
@@ -2,8 +2,6 @@ package com.example.demo.form;
 
 import java.time.LocalDate;
 
-import javax.validation.constraints.NotNull;
-
 import org.springframework.format.annotation.DateTimeFormat;
 
 import lombok.Data;
@@ -12,7 +10,7 @@ import lombok.Data;
 public class BaseDateForm {
 	
 	@DateTimeFormat(pattern="yyyy/MM/dd")
-	@NotNull(message="yyyy/MM/ddの形で入力して下さい")
+	//@NotNull(message="yyyy/MM/ddの形で入力して下さい")
 	private LocalDate baseDate;
 	
 }
