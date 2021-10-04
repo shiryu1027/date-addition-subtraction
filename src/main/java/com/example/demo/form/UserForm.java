25447ecd936b033c8fbcf7621846a5ebb7d96c64
shiryu1027 <asmttmcz.1027@outlook.jp>
2021-10-04 14:34:36 +0900

    UserFormクラスのバリデーションテストの作成

--- src/main/java/com/example/demo/form/UserForm.java
+++ src/main/java/com/example/demo/form/UserForm.java
@@ -1,6 +1,6 @@
 package com.example.demo.form;
 
-import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
 
 import org.hibernate.validator.constraints.Length;
 
@@ -12,14 +12,16 @@ public class UserForm {
 	private int userId;
 	
 	@Length(min=1, max=50, message="50文字以内のメールアドレスを入力して下さい")
-	@NotEmpty(message="メールアドレス必須項目です")
+	//@NotEmpty(message="メールアドレス必須項目です") // lengthと被っているためいらないかも
 	private String mailAddress;
 	
-	@Length(min=1, max=40, message="40文字以内のニックネームを入力して下さい")
-	@NotEmpty(message="ユーザーは必須項目です")
+	@Length(min=1, max=10, message="10文字以内のユーザーネームを入力して下さい")
+	//@NotEmpty(message="ユーザーは必須項目です")
 	private String username;
 	
-	@NotEmpty(message="パスワードは必須項目です")
+	@Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]{10,25}$",
+			message = "パスワードは大小英文字(a-z、A-Z)、数字(0-9)の3種を必ず一回は使用して下さい")
+	//@NotEmpty(message="パスワードは必須項目です")
 	private String password;
 	
 	private String role;
---
+++ src/test/java/com/example/demo/form/UserFormTest.java
@@ -0,0 +1,153 @@
+package com.example.demo.form;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.validation.BindException;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.Validator;
+
+@SpringBootTest
+class UserFormTest{
+	
+	@Autowired
+	Validator validator;
+	
+	private UserForm testUserForm = new UserForm();
+    private BindingResult bindingResult = new BindException(testUserForm, "userForm");
+	
+    @Nested
+    class メールアドレスのチェック {
+    	
+    	@BeforeEach
+    	void setup() {
+    		// 他のフィールドには正常値をセット
+    		testUserForm.setPassword("Password04049");
+    		testUserForm.setUsername("ユーザー");
+    	}
+    	
+    	@ParameterizedTest
+        @ValueSource(strings = {"user@gmail.com",
+        		"abcdefghijklmnopqrstuvwxyz@gmail.com",
+        		"fsaglasjglweFFAogo31314ewhowegfhoeghow@yahoo.co.jp"})
+    	void 正常系(String str) {
+        	testUserForm.setMailAddress(str);
+            // テスト実施
+            validator.validate(testUserForm, bindingResult);
+            // 結果検証
+            assertThat(bindingResult.getFieldError()).isNull();
+    	}
+    	
+    	@ParameterizedTest
+        @ValueSource(strings = {"user1292194214124214214212142141232141221@gmail.com",
+        		"abcdefghijklmnopqrstuvwxyz1234567890fff@ezweb.ne.jp",
+        		"fsaglasjglweFFAogo31314ewhowegfhoeghowf@yahoo.co.jp"})
+    	void 異常系_文字数不正(String str) {
+        	testUserForm.setMailAddress(str);
+            // テスト実施
+            validator.validate(testUserForm, bindingResult);
+            // 結果検証
+            assertThat(bindingResult.getFieldError().toString()).contains("50文字以内のメールアドレスを入力して下さい");
+    	}
+    }
+    
+    @Nested
+    class パスワードのチェック {
+    	@BeforeEach
+    	void setup() {
+    		// 他のフィールドには正常値をセット
+    		testUserForm.setMailAddress("user@gmail.com");
+    		testUserForm.setUsername("ユーザー");
+    	}
+    	
+    	@ParameterizedTest
+        @ValueSource(strings = {"password123Agrehrafs1Sfef",
+        		"1kihiAks3v",
+        		"gewee8ojofkew4A"})
+    	void 正常系(String str) {
+        	testUserForm.setPassword(str);
+            // テスト実施
+            validator.validate(testUserForm, bindingResult);
+            // 結果検証
+            assertThat(bindingResult.getFieldError()).isNull();
+    	}
+    	
+    	@ParameterizedTest
+        @ValueSource(strings = {"pgu8JU3kf",
+        		"gea8NJH8gihfl7keukn7fJi8rj",})
+    	void 異常系_文字数不正(String str) {
+        	testUserForm.setPassword(str);
+            // テスト実施
+            validator.validate(testUserForm, bindingResult);
+            // 結果検証
+            assertThat(bindingResult.getFieldError().toString())
+            	.contains("パスワードは大小英文字(a-z、A-Z)、数字(0-9)の3種を必ず一回は使用して下さい");
+    	}
+    	
+    	@ParameterizedTest
+        @ValueSource(strings = {"password123agrehrafs1sfef",
+        		"feegsdkihiAksv",
+        		"AHRGSAG4325DGE",
+        		"pgaasdgadfdsaf",
+        		"32425454326624",
+        		"BDFHHRHREHREERESDFEH"})
+    	void 異常系_文字種不正(String str) {
+        	testUserForm.setPassword(str);
+            // テスト実施
+            validator.validate(testUserForm, bindingResult);
+            // 結果検証
+            assertThat(bindingResult.getFieldError().toString())
+            	.contains("パスワードは大小英文字(a-z、A-Z)、数字(0-9)の3種を必ず一回は使用して下さい");
+    	}
+    	
+    	@ParameterizedTest
+        @ValueSource(strings = {"psaokgniefiiwefewofnowenfe",
+        		"3523grgert453ko9dsokodsfo9",
+        		"AHRGSAGG4"})
+    	void 異常系_文字数不正かつ文字種不正(String str) {
+        	testUserForm.setPassword(str);
+            // テスト実施
+            validator.validate(testUserForm, bindingResult);
+            // 結果検証
+            assertThat(bindingResult.getFieldError().toString())
+            	.contains("パスワードは大小英文字(a-z、A-Z)、数字(0-9)の3種を必ず一回は使用して下さい");
+    	}	
+    }
+    
+    @Nested
+    class ユーザーネームのチェック {
+    	
+    	@BeforeEach
+    	void setup() {
+    		// 他のフィールドには正常値をセット
+    		testUserForm.setMailAddress("user@gmail.com");
+    		testUserForm.setPassword("Password04049");
+    	}
+    	
+    	@ParameterizedTest
+        @ValueSource(strings = {"k", "ユーザー123456"})
+    	void 正常系(String str) {
+        	testUserForm.setUsername(str);
+            // テスト実施
+            validator.validate(testUserForm, bindingResult);
+            // 結果検証
+            assertThat(bindingResult.getFieldError()).isNull();
+    	}
+    	
+    	@ParameterizedTest
+        @ValueSource(strings = {"", "ユーザー1234567"})
+    	void 異常系_文字数不正(String str) {
+        	testUserForm.setUsername(str);
+            // テスト実施
+            validator.validate(testUserForm, bindingResult);
+            // 結果検証
+            assertThat(bindingResult.getFieldError().toString())
+            	.contains("10文字以内のユーザーネームを入力して下さい");
+    	}	
+    }
+}
